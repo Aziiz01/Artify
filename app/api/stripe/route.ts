710a3379d7f18ec029cb886a1659cce3fab3ace3
Aziiz01 <mohamedaziz.nacib@esprit.tn>
2023-10-08 19:12:59 +0100

    fixedStripeWithDB

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,20 +1,61 @@
 import { stripe } from "@/lib/stripe";
-import { NextResponse, NextRequest } from "next/server";
-// implemnt user verif and database verif fok ala zeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeby
-export async function POST (request : Request) {
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+import { auth, currentUser } from "@clerk/nextjs";
+import { absoluteUrl } from "@/lib/utils";
+
+const creditsUrl = absoluteUrl("/credits");
+
+export async function POST(request: Request) {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
+
+    if (!userId || !user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: creditsUrl,
+      });
+
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    }
+
     let data = await request.json();
-    let priceId = data.priceId
+    let priceId = data.priceId;
+
+    if (!priceId) {
+      return new NextResponse("Price ID is required", { status: 400 });
+    }
+
     const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                price: priceId,
-                quantity: 1
-            }
-        ],
-      mode: 'subscription',
-      success_url: 'http://localhost:3000/credits',
-      cancel_url: 'http://localhost:3000/credtis'
-    })
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      success_url: creditsUrl,
+      cancel_url: creditsUrl,
+      
+      metadata: {
+        userId,
+      },
+    });
 
     return NextResponse.json(session.url)
+      } catch (error) {
+    console.log("[STRIPE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
