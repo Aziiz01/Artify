7e2c0e58fc06ddd92492d6d3b06742e0d053d873
salahbnh <salah.bounouh55@gmail.com>
2024-01-16 16:12:16 +0100

    testingPage works successfully, missing some touches

--- app/(dashboard)/(routes)/testingPage/page.tsx
+++ app/(dashboard)/(routes)/testingPage/page.tsx
@@ -1,79 +1,198 @@
 import { revalidatePath } from 'next/cache';
 import React from 'react'
 import SideBarForm from '@/app/(dashboard)/(routes)/testingPage/sideBarForm';
+import { SDXL } from '../../../api/sdxl-v1/route'; // Import the function
+import { string } from 'zod';
+import axios from 'axios';
+import Image from "next/image";
+
+export interface imageDataResponse{
+  artifacts: Array<{
+    base64: string
+    seed: number
+    finishReason: string
+  }>
+}
 
 export interface imageData{
-    prompt : string,
-    style : string|undefined,
-    samples : string|undefined,
-    algoModel:string|undefined,
-    negativePrompt:string|undefined,
-    width:string|undefined,
-    height:string|undefined,
-    cfg:string|undefined,
-    steps:string|undefined,
-    seed:string|undefined
+    text_prompts: string,
+    style_preset : string,
+    samples : number,
+    algoModel:string,
+    width:number,
+    height:number,
+    cfg_scale:number,
+    steps:number,
+    seed:number
+    negativePrompt: string
+    //fast_count:number|undefined
   }
   
   const generateImageDatas: imageData[]= [];
+  let theImageData: imageData = {
+    text_prompts:"",
+    style_preset: "",
+    samples: 0,
+    algoModel: "",
+    width:1024 ,
+    height: 1024,
+    cfg_scale: 7,
+    steps: 30,
+    seed: 0,
+    negativePrompt:"",
+  };
+   
+  let theImageDataResponse:imageDataResponse ={
+    artifacts: [], // Start with an empty array of artifacts
+  };
 
+  //const htmlImgElements: HTMLImageElement[] = [];
+  let base64Images: string[]=[];
+  
   export default function Test() {
     
     async function generateImage(data:FormData){
         "use server"
+        const userId= data.get("userId")?.toString();
         const prompt= data.get("prompt")?.toString();
         const style = data.get("style")?.toString();
-        const samples = data.get("samples")?.toString();
-        const algoModel= data.get("style")?.toString();
-        const negativePrompt= data.get("algoModel")?.toString();
-        const width= data.get("width")?.toString();
-        const height= data.get("height")?.toString();
-        const cfg= data.get("cfg")?.toString();
-        const steps= data.get("steps")?.toString();
-        const seed= data.get("seed")?.toString();
-        
+        const samples = data.get("samples ") ? parseInt(data.get("samples")!.toString(), 10) : undefined;
+        const algoModel= data.get("algoModel")?.toString();
+        const negativePrompt= data.get("negativePrompt")?.toString();
+        const width = data.get("width") ? parseInt(data.get("width")!.toString(), 10) : undefined;
+        const height = data.get("height") ? parseInt(data.get("height")!.toString(), 10) : undefined;
+        const cfg = data.get("cfg") ? parseInt(data.get("cfg")!.toString(), 10) : undefined;
+        const steps = data.get("steps") ? parseInt(data.get("steps")!.toString(), 10) : undefined;
+        const seed = data.get("seed") ? parseInt(data.get("seed")!.toString(), 10) : undefined;
+        const fast_count = data.get("fast_count") ? parseInt(data.get("fast_count")!.toString(), 10) : undefined;
+
         if(!prompt || !algoModel) return;
         const newImageData: imageData ={
-          prompt,
-          style,
-          samples ,
-          algoModel,
-          negativePrompt,
-          width,
-          height,
-          cfg,
-          steps,
-          seed
+          text_prompts:prompt,
+          algoModel: algoModel? algoModel: "stable-diffusion-v1-6",
+          style_preset : style? style : "",
+          cfg_scale:cfg? cfg : 7,
+          height: height? height : 1024 ,
+          width: width? width: 1024,
+          steps: steps? steps: 30,
+          samples : samples? samples : 1,
+          seed:seed? seed: 0,
+          negativePrompt: negativePrompt? negativePrompt : ""
         }
+        theImageData = newImageData;
         generateImageDatas.push(newImageData);
-        revalidatePath('/testingPage');
+
+        try {
+          const path =`https://api.stability.ai/v1/generation/${theImageData.algoModel}/text-to-image`;
+          const apiKey = "sk-BaEwtEhRzUIH4odaU5jcjFcvTr9CH2xsZNElGYRWa5BLycos";
+          const headers = {
+            Accept: "application/json",
+            Authorization: `Bearer ${apiKey}`,
+            "Content-Type": "application/json", 
+          };
+          
+          const body: {
+            steps: number;
+            width: number;
+            height: number;
+            seed: number;
+            cfg_scale: number;
+            samples: number;
+            text_prompts: { text: string; weight: number }[];
+            style_preset?: string; 
+          } = {
+            steps: theImageData.steps,
+            width: theImageData.width,
+            height: theImageData.height,
+            seed: theImageData.seed,
+            cfg_scale: theImageData.cfg_scale,
+            samples: theImageData.cfg_scale,
+            text_prompts: [
+              {
+                text: theImageData.text_prompts,
+                weight: 1,
+              },
+            ],
+          };
+          if (theImageData.style_preset !== '') {
+            body.style_preset = theImageData.style_preset;
+          }
+          if (theImageData.negativePrompt!== '') {
+            body.text_prompts.push({
+              text: theImageData.negativePrompt,
+              weight: -1,
+            });
+          }
+          const response = await fetch(path, {
+            headers,
+            method: "POST",
+            body: JSON.stringify(body),
+            cache: "no-store" 
+          });
+
+          if (!response.ok) {
+            throw new Error(`Non-200 response: ${await response.text()}`)
+            console.log("image generated with success"+ response);
+          }
+          
+          const responseJSON = await response.json();
+
+          base64Images = responseJSON.artifacts.map((image:any) => image.base64);
+
+          // responseJSON.artifacts.forEach((image: any, index: any) => {
+          //   // Construct the data URL for the image
+          //   const dataUrl = `data:image/png;base64,${image.base64}`;
+    
+          //   // Create an HTML img element
+          //   const imgElement = document.createElement('img');
+          //   imgElement.src = dataUrl;
+          //   imgElement.alt = `Generated Image ${index + 1}`;
+    
+          //   // Push the img element to the array
+          //   htmlImgElements.push(imgElement);
+            
+//          });
+          
+          revalidatePath('/testingPage');
+          return base64Images;
+
+        }catch(error) {
+          console.error('Failed to generate Text-To-Image, Error:', error);
+          return null;
+        }
       }
+
+
     return (
         <div style={{
             display:'grid',
             height:'2000px',
-            gridTemplateRows:'50% 50%'
+            gridTemplateColumns:'50% 50%'
           }}>
-             <div className="px-4 lg:px-8 bg-transparent" style={{ height:'850px'  }}>
-                <div className="bg-white rounded-lg p-4 mb-4 mt-4">
-                   <div>
-
-                   </div>
-                </div>
-             </div>
 
              
         <div className='side-bar'>
             <SideBarForm generateImage={generateImage}/>
         </div>
-        <div className='generate-section'>
-            {generateImageDatas.map((data,index) =>(
-                <div key={index}>
-                    <h2>{data.prompt}</h2>
-                    <h2>{data.style}</h2>
-                </div>
-            ))}
-        </div>
+        <div className="px-4 lg:px-8 bg-transparent" style={{ height:'850px'  }}>
+           <div className="bg-white rounded-lg p-4 mb-4 mt-4">
+              <h2> theImageDataprompt </h2>
+              <h2> {theImageData.samples} </h2>
+            </div>
+            <div>
+              <h1>Artifacts</h1>
+              <div>
+              {base64Images.map((base64, index) => (
+                  <Image 
+                  key={index} 
+                  src={`data:image/png;base64,${base64}`} 
+                  alt={`Generated Image ${index + 1}`}             
+                  height={1024}
+                  width={1024}/>
+                ))}
+              </div>
+            </div>
+         </div>
     </div>
   )
 }
--- app/(dashboard)/(routes)/testingPage/sideBarForm.tsx
+++ app/(dashboard)/(routes)/testingPage/sideBarForm.tsx
@@ -1,87 +1,100 @@
 "use client"
-import React ,{useState} from 'react'
+import React,{useState} from 'react'
 
-type GenerateImageProp = (data: FormData) => Promise<void>;
+export interface GenerateImageProp{
+    generateImage: (data: FormData)=> Promise<String[]|null|undefined>;
+}
 
-function SideBarForm({generateImage}:{generateImage:GenerateImageProp}) {
-  const [prompt, setPrompt] = useState<string>("");
-  const [style, setStyle] = useState<string | undefined>(undefined);
-  const [samples, setSamples] = useState<string | undefined>(undefined);
-  const [algoModel, setAlgoModel] = useState<string | undefined>(undefined);
-  const [negativePrompt, setNegativePrompt] = useState<string | undefined>(undefined);
-  const [width, setWidth] = useState<string | undefined>(undefined);
-  const [height, setHeight] = useState<string | undefined>(undefined);
-  const [cfg, setCfg] = useState<string | undefined>(undefined);
-  const [steps, setSteps] = useState<string | undefined>(undefined);
-  const [seed, setSeed] = useState<string | undefined>(undefined);
+const SideBarForm: React.FC<GenerateImageProp> = ({ generateImage }) => {
+    const [prompt, setPrompt] = useState<string>("");
+    const [style, setStyle] = useState<string | undefined>(undefined);
+    const [samples, setSamples] = useState<string | undefined>(undefined);
+    const [algoModel, setAlgoModel] = useState<string | undefined>(undefined);
+    const [negativePrompt, setNegativePrompt] = useState<string | undefined>(undefined);
+    const [width, setWidth] = useState<string | undefined>(undefined);
+    const [height, setHeight] = useState<string | undefined>(undefined);
+    const [cfg, setCfg] = useState<string | undefined>(undefined);
+    const [steps, setSteps] = useState<string | undefined>(undefined);
+    const [seed, setSeed] = useState<string | undefined>(undefined);
 
   return (
     <div>
-      <form action={generateImage}>
-      <input
-        type="text"
-        name="prompt"
-        value={prompt}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="style"
-        value={style}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="samples"
-        value={samples}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="algoModel"
-        value={algoModel}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="negativePrompt"
-        value={negativePrompt}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="width"
-        value={width}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="height"
-        value={height}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="cfg"
-        value={cfg}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="steps"
-        value={steps}
-        style={{ display: 'none' }}
-      />
-      <input
-        type="text"
-        name="seed"
-        value={seed}
-        style={{ display: 'none' }}
-      />
-      <button type="submit">Generate</button>
-    </form>
+        <form action={generateImage}>
+          {/* Use "hidden" style to hide the inputs visually */}
+          <h2>Prompt</h2>          
+          <input
+            type="text"
+            name="prompt"
+            //value={prompt}
+            //style={{ display: 'none' }}
+          />
+          <h2>style</h2>          
+          <input
+            type="text"
+            name="style"
+            //value={style}
+            //style={{ display: 'none' }}
+          />
+          <h2>samples</h2>          
+          <input
+            type="text"
+            name="samples"
+           //value={samples}
+            //style={{ display: 'none' }}
+          />
+          <h2>algoModel</h2>          
+          <input
+            type="text"
+            name="algoModel"
+            //value={algoModel}
+            //style={{ display: 'none' }}
+          />
+          <h2>negativePrompt</h2>          
+          <input
+            type="text"
+            name="negativePrompt"
+            //value={negativePrompt}
+            //style={{ display: 'none' }}
+          />
+          <h2>width</h2>          
+          <input
+            type="text"
+            name="width"
+            //value={width}
+           // style={{ display: 'none' }}
+          />
+          <h2>height</h2>          
+          <input
+            type="text"
+            name="height"
+           // value={height}
+            //style={{ display: 'none' }}
+          />
+          <h2>cfg</h2>          
+          <input
+            type="text"
+            name="cfg"
+           //value={cfg}
+            //style={{ display: 'none' }}
+          />
+          <h2>steps</h2>          
+          <input
+            type="text"
+            name="steps"
+            //value={steps}
+            //style={{ display: 'none' }}
+          />
+          <h2>seed</h2>          
+          <input
+            type="text"
+            name="seed"
+            //value={seed}
+            //style={{ display: 'none' }}
+          />
+          <button type="submit">Generate</button>
+        </form>
     </div>
   )
 }
 
-export default SideBarForm;
+export default SideBarForm
